296b05a715b8f65be2c0b324dad543ee94b9cfb6
Marco Cristofani <allievo_cristofani.m@itsumbria.it>
2020-09-30 11:20:21 +0200

    Aggiungere i file di progetto.

---
+++ ITSUmbria2020.OnlineGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITSUmbria2020.OnlineGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ITSUmbria2020.OnlineGameLibrary.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ITSUmbria2020.OnlineGameLibrary.Models
+{
+    public class Class1
+    {
+    }
+}
